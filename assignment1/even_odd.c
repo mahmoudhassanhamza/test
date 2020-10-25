ffbe6e91cb2a981fe8c24dc7410c817884d1fa73
mahmoud hamza <mahmoudhasssanhamza@gmail.com>
2020-10-26 00:08:15 +0100

    consonant vs vowels

--- assignment1/even_odd.c
+++ assignment1/even_odd.c
@@ -1,26 +1,28 @@
-#include "stdio.h"
-
-int main(){
-	int x;
-		fflush(stdout);
-		printf("enter a number \n");
-		fflush(stdin);
-		scanf("%d",&x);
-		if(x%2)
-		{
-			fflush(stdout);
-			printf("%d is odd",x);
-
-		}
-		else
-		{
-			fflush(stdout);
-			printf("%d is even",x);
-
-		}
-
-	return 0;
-
-
-}
-
+//#include "stdio.h"
+//
+//int main(){
+//
+//
+//	int x;
+//		fflush(stdin);
+//		printf("enter a number \n");
+//		fflush(stdout);
+//		scanf("%d",&x);
+//		fflush(stdin);
+//
+//		if(x%2)
+//		{
+//			printf("%d is odd",x);
+//			fflush(stdout);
+//
+//
+//		}
+//		else
+//		{
+//			printf("%d is even",x);
+//			fflush(stdout);
+//		}
+//	return 0;
+//
+//}
+//
