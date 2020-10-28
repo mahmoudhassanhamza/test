05da05f8d433c4cf734aff4c7cd9c7425ee253ec
mahmoud hamza <mahmoudhasssanhamza@gmail.com>
2020-10-28 17:35:05 +0100

    adding new elements to homework3

---
+++ assignment3/homework3i/headers.h
@@ -0,0 +1,14 @@
+/*
+ * headers.h
+ *
+ *  Created on: Oct 28, 2020
+ *      Author: mahmo
+ */
+
+#ifndef HEADERS_H_
+#define HEADERS_H_
+
+
+void Sum();
+void Average();
+#endif /* HEADERS_H_ */
---
+++ assignment3/homework3i/main.c
@@ -0,0 +1,48 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 28, 2020
+ *      Author: mahmo
+ */
+
+
+/*
+ ============================================================================
+ Name        : homework3.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "headers.h"
+int main(void)
+{
+//
+//	int a[2][2],b[2][2],temp[2][2];
+	int x=2;
+//	int i,j,k;
+	switch(x)
+	{
+	case 1:
+	{
+		Sum();
+		break;
+	}
+
+	case 2:
+	{
+		Average();
+		break;
+	}
+
+
+	}
+
+
+
+
+}
